de12feeba86ad9e83d090d5193db1594ace0765b
Padmanabha Das <82430454+chayan-1906@users.noreply.github.com>
2021-10-20 14:46:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MySplashScreen(),
     );
   }
